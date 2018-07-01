7412e1d63887b0b5d43af8580b346bd0a04ddb79
LAPTOP-TCSCID23\dejan <dejan.luznic@gmail.com>
2018-07-01 17:35:02 +0200

    minor fix: register form password input now has min lenght

--- app/forms.py
+++ app/forms.py
@@ -5,13 +5,13 @@ from wtforms.validators import InputRequired, Email, Length
 #The login form
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[InputRequired(),Email(message='Enter a valid email'), Length(max = 60)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=30)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=30)])
     remember = BooleanField('Remember me')
 
 #Account registration  form
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email(message='Enter a valid email'),Length(max=60)])
     username = StringField('Username',validators=[InputRequired(), Length(min=4, max = 20)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(max=30)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4,max=30)])
     firstname = StringField('Firstname',validators=[InputRequired(), Length(max = 20)])
     lastname = StringField('Lastname',validators=[InputRequired(), Length(max = 20)])
